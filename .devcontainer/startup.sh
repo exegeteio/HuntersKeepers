b162319c1468bf50ee22cebfabf562697a3ae53b
Jack M <exegete@hey.com>
2021-02-14 12:51:50 +0000

    Prefer simple (echo) over comples (termd) since no colour output.

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,21 +4,21 @@
 set -e verbose
 
 # Gems required for VS Code extensions.
-termd -s "# Installing Solargraph and Rubocop"
+echo "# Installing Solargraph and Rubocop"
 sudo gem install solargraph rubocop
 
 # Make sure the bundle is up to date.
-termd -s "# Updating bundle"
+echo "# Updating bundle"
 docker-compose run --rm web bundle install -j4
 
 # Make sure the node_modules are up to date.
-termd -s "# Updating yarn"
+echo "# Updating yarn"
 docker-compose run --rm web yarn install --check-files
 
 # Ensure the database is set up and running.
-termd -s "# Database Setup"
+echo "# Database Setup"
 docker-compose run --rm web bundle exec rails db:setup db:seed
 
 # Start up the services automagically.
-termd -s "# Starting Services"
+echo "# Starting Services"
 docker-compose up -d
